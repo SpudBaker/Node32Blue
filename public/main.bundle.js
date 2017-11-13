bd805fdb8881a85e5553a8f9e54f1c8c5c49064d
SpudBaker <spudbaker@gmail.com>
2017-11-13 21:24:31 +0000

    new angular build

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -20,7 +20,7 @@ webpackEmptyAsyncContext.id = "../../../../../src/$$_lazy_route_resource lazy re
 /***/ "../../../../../src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<mat-toolbar color=\"primary\">\n    <span>32 Blue</span>\n    <span class=\"fill-remaining-space\"></span>\n    <span>Logo</span>\n</mat-toolbar>\n<mat-sidenav-container>\n<mat-sidenav mode=\"side\" opened=\"true\" disableClose>\n  <!-- sidenav content -->\n  <div>\n    <a mat-button class=\"button-width\" routerLink = \"home\">Home</a><br/>\n    <a mat-button class=\"button-width\" routerLink = \"company\">Company</a><br/>\n    <a mat-button class=\"button-width\">Services</a><br/>\n    <a mat-button class=\"button-width\">References</a><br/>\n  </div>\n</mat-sidenav>\n<!-- primary content -->\n  <div class=\"fill-content\"><router-outlet></router-outlet></div>\n</mat-sidenav-container>\n\n\n"
+module.exports = "<mat-toolbar color=\"primary\">\n    <span>32 Blue</span>\n    <span class=\"fill-remaining-space\"></span>\n    <span>Logo</span>\n</mat-toolbar>\n<mat-sidenav-container>\n<mat-sidenav mode=\"side\" opened=\"true\" disableClose>\n  <!-- sidenav content -->\n  <div>\n    <a mat-button class=\"button-width\" routerLink = \"home\">Home</a><br/>\n    <a mat-button class=\"button-width\" routerLink = \"company\">Company</a><br/>\n    <a mat-button class=\"button-width\" routerLink = \"services\">Services</a><br/>\n    <a mat-button class=\"button-width\" routerLink = \"technology\">Technology</a><br/>\n    <a mat-button class=\"button-width\" routerLink = \"method\">Method</a><br/>\n    <a mat-button class=\"button-width\" routerLink = \"examples\">Examples</a><br/>\n    <a mat-button class=\"button-width\" routerLink = \"references\">References</a><br/>\n    <a mat-button class=\"button-width\" routerLink = \"contact\">Contact</a><br/>\n  </div>\n</mat-sidenav>\n<!-- primary content -->\n  <div class=\"fill-content\"><router-outlet></router-outlet></div>\n</mat-sidenav-container>\n\n\n"
 
 /***/ }),
 
@@ -68,6 +68,12 @@ var AppComponent = (function () {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__company_company_component__ = __webpack_require__("../../../../../src/app/company/company.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__pageNotFound_pageNotFound_component__ = __webpack_require__("../../../../../src/app/pageNotFound/pageNotFound.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__home_home_component__ = __webpack_require__("../../../../../src/app/home/home.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__services_services_component__ = __webpack_require__("../../../../../src/app/services/services.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__technology_technology_component__ = __webpack_require__("../../../../../src/app/technology/technology.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_11__method_method_component__ = __webpack_require__("../../../../../src/app/method/method.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_12__examples_examples_component__ = __webpack_require__("../../../../../src/app/examples/examples.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_13__references_references_component__ = __webpack_require__("../../../../../src/app/references/references.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_14__contact_contact_component__ = __webpack_require__("../../../../../src/app/contact/contact.component.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -83,9 +89,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
+
+
+
+
+
+
 var appRoutes = [
     { path: 'company', component: __WEBPACK_IMPORTED_MODULE_6__company_company_component__["a" /* CompanyComponent */] },
     { path: 'home', component: __WEBPACK_IMPORTED_MODULE_8__home_home_component__["a" /* HomeComponent */] },
+    { path: 'services', component: __WEBPACK_IMPORTED_MODULE_9__services_services_component__["a" /* ServicesComponent */] },
+    { path: 'technology', component: __WEBPACK_IMPORTED_MODULE_10__technology_technology_component__["a" /* TechnologyComponent */] },
+    { path: 'method', component: __WEBPACK_IMPORTED_MODULE_11__method_method_component__["a" /* MethodComponent */] },
+    { path: 'examples', component: __WEBPACK_IMPORTED_MODULE_12__examples_examples_component__["a" /* ExamplesComponent */] },
+    { path: 'references', component: __WEBPACK_IMPORTED_MODULE_13__references_references_component__["a" /* ReferencesComponent */] },
+    { path: 'contact', component: __WEBPACK_IMPORTED_MODULE_14__contact_contact_component__["a" /* ContactComponent */] },
     { path: '',
         redirectTo: '/home',
         pathMatch: 'full'
@@ -101,7 +119,13 @@ var AppModule = (function () {
                 __WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* AppComponent */],
                 __WEBPACK_IMPORTED_MODULE_8__home_home_component__["a" /* HomeComponent */],
                 __WEBPACK_IMPORTED_MODULE_6__company_company_component__["a" /* CompanyComponent */],
-                __WEBPACK_IMPORTED_MODULE_7__pageNotFound_pageNotFound_component__["a" /* PageNotFoundComponent */]
+                __WEBPACK_IMPORTED_MODULE_9__services_services_component__["a" /* ServicesComponent */],
+                __WEBPACK_IMPORTED_MODULE_10__technology_technology_component__["a" /* TechnologyComponent */],
+                __WEBPACK_IMPORTED_MODULE_11__method_method_component__["a" /* MethodComponent */],
+                __WEBPACK_IMPORTED_MODULE_12__examples_examples_component__["a" /* ExamplesComponent */],
+                __WEBPACK_IMPORTED_MODULE_13__references_references_component__["a" /* ReferencesComponent */],
+                __WEBPACK_IMPORTED_MODULE_7__pageNotFound_pageNotFound_component__["a" /* PageNotFoundComponent */],
+                __WEBPACK_IMPORTED_MODULE_14__contact_contact_component__["a" /* ContactComponent */]
             ],
             imports: [
                 __WEBPACK_IMPORTED_MODULE_2__angular_router__["a" /* RouterModule */].forRoot(appRoutes),
@@ -125,7 +149,7 @@ var AppModule = (function () {
 /***/ "../../../../../src/app/company/company.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<br/>\r\n<br/>\r\nghjhdfguioipkljhgggcvhj"
+module.exports = "<div>\r\n        32 Blue builds high quality, affordable, low maintenance business applications for the SME market. \r\n        <br/>\r\n        <br/>\r\n        The company was formed in 2017 by Richard Baker, BSc(Hons), Prince 2 practitioner and ITIL expert. \r\n        Richard has worked in IT for 20 years in Blue Chip and SME organisations most recently as a Head of IT. \r\n    <br/>\r\n    <br/>\r\n        The characteristics that set 32 Blue apart are:<br/> \r\n        <ul>\r\n            <li>Customer engagement is key</li>\r\n            <li>Solutions must not be just functionally rich but must also be responsive, available, and trouble free allowing for satisfied and effective users</li>\r\n            <li>Use of enterpise standard open-source technology</li>\r\n            <li>Quality is vital</li>\r\n            <li>A transparent cost and delivery model with appropriate project management</li>\r\n        </ul>\r\n</div>"
 
 /***/ }),
 
@@ -158,10 +182,82 @@ var CompanyComponent = (function () {
 
 /***/ }),
 
+/***/ "../../../../../src/app/contact/contact.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div>\r\n        Contact\r\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/contact/contact.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ContactComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var ContactComponent = (function () {
+    function ContactComponent() {
+    }
+    ContactComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'app-contact',
+            template: __webpack_require__("../../../../../src/app/contact/contact.component.html")
+        })
+    ], ContactComponent);
+    return ContactComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/examples/examples.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div>\r\n        Examples\r\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/examples/examples.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ExamplesComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var ExamplesComponent = (function () {
+    function ExamplesComponent() {
+    }
+    ExamplesComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'app-examples',
+            template: __webpack_require__("../../../../../src/app/examples/examples.component.html")
+        })
+    ], ExamplesComponent);
+    return ExamplesComponent;
+}());
+
+
+
+/***/ }),
+
 /***/ "../../../../../src/app/home/home.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div>\r\n<br/>\r\n    32 Blue delivers cost effective, reliable IT solutions for the SME market. Applications that are;\r\n\r\n    <ul>\r\n        <li>Designed to run in a browser using the latest technologies</li>\r\n        <li>Cloud based</li>\r\n        <li>Mobile device and PC capable</li>\r\n        <li>Highly functional and data driven</li>\r\n        <li>Secure</li>\r\n        <li>Without software installation and licensing costs</li>\r\n        <li>Capable of supporting any number of users simultaneously</li>\r\n        <li>Optimised for speed and responsiveness allowing for a great user experience</li>\r\n    </ul>\r\n<br/>\r\n<br/>\r\n<br/>\r\n<br/>\r\n<br/>\r\n</div>"
+module.exports = "<div>\r\n    32 Blue delivers cost effective, reliable IT solutions for the SME market. Applications that are;\r\n\r\n    <ul>\r\n        <li>Designed to run in a browser using the latest technologies</li>\r\n        <li>Cloud based</li>\r\n        <li>Mobile device and PC capable</li>\r\n        <li>Highly functional and data driven</li>\r\n        <li>Secure</li>\r\n        <li>Without software installation and licensing costs</li>\r\n        <li>Capable of supporting any number of users simultaneously</li>\r\n        <li>Optimised for speed and responsiveness allowing for a great user experience</li>\r\n    </ul>\r\n<br/>\r\n<br/>\r\n<br/>\r\n<br/>\r\n<br/>\r\n</div>"
 
 /***/ }),
 
@@ -188,6 +284,42 @@ var HomeComponent = (function () {
         })
     ], HomeComponent);
     return HomeComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/method/method.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div>\r\n        Method\r\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/method/method.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MethodComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var MethodComponent = (function () {
+    function MethodComponent() {
+    }
+    MethodComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'app-method',
+            template: __webpack_require__("../../../../../src/app/method/method.component.html")
+        })
+    ], MethodComponent);
+    return MethodComponent;
 }());
 
 
@@ -224,6 +356,114 @@ var PageNotFoundComponent = (function () {
         })
     ], PageNotFoundComponent);
     return PageNotFoundComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/references/references.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div>\r\n        References\r\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/references/references.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ReferencesComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var ReferencesComponent = (function () {
+    function ReferencesComponent() {
+    }
+    ReferencesComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'app-references',
+            template: __webpack_require__("../../../../../src/app/references/references.component.html")
+        })
+    ], ReferencesComponent);
+    return ReferencesComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/services/services.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div>\r\n       Services\r\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/services/services.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ServicesComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var ServicesComponent = (function () {
+    function ServicesComponent() {
+    }
+    ServicesComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'app-services',
+            template: __webpack_require__("../../../../../src/app/services/services.component.html")
+        })
+    ], ServicesComponent);
+    return ServicesComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/technology/technology.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div>\r\n        Technology\r\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/technology/technology.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return TechnologyComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var TechnologyComponent = (function () {
+    function TechnologyComponent() {
+    }
+    TechnologyComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'app-technology',
+            template: __webpack_require__("../../../../../src/app/technology/technology.component.html")
+        })
+    ], TechnologyComponent);
+    return TechnologyComponent;
 }());
 
 
